2579d1855fea1b117dddee344f1b6bfff4472f81
pauruigar6 <pauruigar6@alum.us.es>
2023-12-01 13:17:18 +0100

    feat: add backend folder

---
+++ caremonitor-backend/src/main/java/com/caremonitor/caremonitor/CaremonitorApplication.java
@@ -0,0 +1,13 @@
+package com.caremonitor.caremonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaremonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaremonitorApplication.class, args);
+	}
+
+}
---
+++ caremonitor-backend/src/test/java/com/caremonitor/caremonitor/CaremonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caremonitor.caremonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaremonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
